6a652255ba4722fc177bd05c090925ae078310cb
= <robert.masen@smartthings.com>
2024-12-26 12:52:49 -0600

    add port argument

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,12 @@ type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
 #[tokio::main]
 async fn main() -> Result {
+    let port = std::env::args().skip(1).next().and_then(|s| {
+        s.parse::<u16>().ok()
+    }).unwrap_or(8080);
     let app = Router::new()
         .route("/delay/:secs", get(delay));
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(("0.0.0.0", port)).await.unwrap();
     Ok(axum::serve(listener, app).await?)
 }
 
