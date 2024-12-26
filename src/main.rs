201f576256c94345a0135ba71025ce50769294cc
= <robert.masen@smartthings.com>
2024-12-26 13:16:00 -0600

    add tls option

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,44 @@
+use std::{net::SocketAddr, path::PathBuf};
+
 use axum::{extract::Path, Router, routing::get};
+use axum_server::tls_rustls::RustlsConfig;
 
 type Result<T = (), E = Box<dyn std::error::Error>> = std::result::Result<T, E>;
 
 #[tokio::main]
 async fn main() -> Result {
-    let port = std::env::args().skip(1).next().and_then(|s| {
+    let mut args = std::env::args().skip(1);
+    let port = args.next().and_then(|s| {
         s.parse::<u16>().ok()
     }).unwrap_or(8080);
+    let cert_dir = args.next().map(PathBuf::from);
+
     let app = Router::new()
         .route("/delay/:secs", get(delay));
-    let listener = tokio::net::TcpListener::bind(("0.0.0.0", port)).await.unwrap();
+    let Some(cert_dir) = cert_dir else {
+        return run_http_server(app, port).await
+    };
+    run_https_server(app, port, cert_dir).await
+}
+
+async fn run_http_server(app: Router, port: u16) -> Result {
+    let listener = tokio::net::TcpListener::bind(("127.0.0.1", port)).await.unwrap();
     Ok(axum::serve(listener, app).await?)
+}
+
+async fn run_https_server(app: Router, port: u16, cert_dir: PathBuf) -> Result {
+    let addr = SocketAddr::from(([127, 0, 0, 1], port));
+    let config = RustlsConfig::from_pem_file(
+        cert_dir
+            .join("cert.pem"),
+        cert_dir
+            .join("key.pem"),
+    )
+    .await
+    ?;
+    Ok(axum_server::bind_rustls(addr, config)
+        .serve(app.into_make_service())
+        .await?)
 }
 
 async fn delay(secs: Path<u64>) {
